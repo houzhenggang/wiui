fd5753f9caed0e05abc4e9dd5b1febe2d6af4253
Baozhu Zuo <zuobaozhu@gmail.com>
2016-10-27 07:48:24 -0700

    use mutl thread to scan wifi

--- src/main.c
+++ src/main.c
@@ -1,22 +1,52 @@
 #include "dialog.h"
 #include "wiui.h"
-
+#include<pthread.h> 
+#include <syslog.h>
 static void sig_handler(int signo)
 {
     int res = 1;
 	exit(res);
 }
+void *scan_func(void *arg)  
+{  
+    struct wifi_describe* wifi_table;
+    wiui *w;    
+    int n;
+    int wifi_table_length = 3;
+    int max_choice;
+    w = get_wifi();
+    wifi_table = (*w->scan)(&wifi_table_length);
+    sleep(5);
+    item_reset(); 
 
+    for (n = 0; n < wifi_table_length;n++) {
+        item_make("%s", wifi_table->ssid);
+        syslog(LOG_INFO, "main: - %s %s \n", wifi_table->ssid, wifi_table->security);
+        item_set_tag('m');
+        wifi_table++;
+    }
+    syslog(LOG_INFO, "main: -wifi_table_length:%d \n", wifi_table_length);
+    max_choice = MIN(get_menu_height(), item_count());
+
+	/* Print the menu */
+	for (n = 0; n < max_choice; n++) {
+		print_item(n, n, 0);
+	}
+    fresh_menu();
+    free(w);
+    free(wifi_table);
+    return ((void *)0);  
+}  
 int main() {
     int s_scroll = 0;
     int ret;
-    struct wifi_describe* wifi_table = (struct wifi_describe*) calloc(1, sizeof(struct wifi_describe));
-    wiui *w;
+    pthread_t scan_tid;  
+
     setlocale(LC_ALL, "");
     signal(SIGINT, sig_handler);
     
-    w = get_wifi();
-    w->scan(wifi_table);
+    pthread_create(&scan_tid, NULL, scan_func, NULL);
+
      
 	if (init_dialog(NULL)) {
 		fprintf(stderr, "Your display is too small to run wiui!\n");
@@ -25,12 +55,9 @@ int main() {
 	}
 
     item_reset();
-    item_make("Helloworld1");
+    item_make("Scan wifi,Please wait ....");
     item_set_tag('m');
-    item_make("Helloworld2");
-    item_set_tag('m');
-    item_make("Helloworld3");
-    item_set_tag('m');
+
 
 
     dialog_menu("WIUI visible","Please set up your wifi !",NULL,&s_scroll);
@@ -40,6 +67,5 @@ int main() {
 	} while (ret == KEY_ESC);
     end_dialog(0, 0);
 
-    free(w);
     return 0;
 }
